2c7a03713038f948153f4dbb72a997624075ab38
saki <srinivasulu@rekonsys.tech>
2024-05-22 11:02:07 +0530

    ss

---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.create, name='create'),
+    path('profile/',views.profile,name='profile'),
+    path('signin/',views.signin,name='signin'),
+    
+    
+    
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create(request):
+    return render(request, 'create.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
